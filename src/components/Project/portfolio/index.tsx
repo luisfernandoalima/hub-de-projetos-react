75d7724f8ae6ecf17a2391191ba6c006015f30c1
Luis Lima <luisfernando.lima2005@gmail.com>
2024-12-21 11:51:29 -0300

    feature/Finishing portfolio page

--- src/components/Project/portfolio/index.tsx
+++ src/components/Project/portfolio/index.tsx
@@ -1,3 +1,4 @@
+import { FaGithub } from "react-icons/fa";
 import { PortfolioCarousel } from "../Carousel/portfolio.tsx";
 
 import "../main.scss";
@@ -8,15 +9,16 @@ export const ProjectPortfolio = () => {
       <section className="project_header">
         <PortfolioCarousel />
         <div className="project_title p-5">
-          <h1 className="font-bold text-5xl mb-3">Portfolio</h1>
-          <h2 className="font-semibold text-3xl mb-3">Meu portfolio pessoal</h2>
+          <h1 className="font-bold text-5xl md:text-4xl lg:text-5xl mb-3">Portfolio</h1>
+          <h2 className="font-semibold text-3xl md:text-2xl lg:text-3xl mb-3">Meu primeiro portfolio pessoal</h2>
           <p className="text-xl">Tecnologias utilizadas:</p>
-          <ul className="list-disc mt-3 ml-7">
+          <ul className="list-disc mt-3 mb-2 ml-2">
             <li>HTML;</li>
             <li>CSS;</li>
             <li>JavaScript;</li>
             <li>Bootstrap.</li>
           </ul>
+          <a href="" target="_blank">Ver Projeto</a>
         </div>
       </section>
 
@@ -35,8 +37,17 @@ export const ProjectPortfolio = () => {
             <li>JavaScript;</li>
             <li>Bootstrap.</li>
           </ul>
-          <p>Junto ao JavaScript, foi utilizada a biblioteca Atropos que permite criar efeitos ao passar o mouse por cima de elementos da página.</p>
-          <p>O Bootstrap, um framework web, também foi utilizado para auxiliar na responsividade do site.</p>
+          <p>
+            Junto ao JavaScript, foi utilizada a biblioteca Atropos que permite
+            criar efeitos ao passar o mouse por cima de elementos da página.
+          </p>
+          <p>
+            O Bootstrap, um framework web, também foi utilizado para auxiliar na
+            responsividade do site.
+          </p>
+          <a href="" target="_blank">
+            <FaGithub /> Link do Repositório
+          </a>
         </article>
       </section>
     </>
